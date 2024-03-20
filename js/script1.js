a8892e1703107265e2f8062da4e01a1020bccbe8
loryy9 <102917754+loryy9@users.noreply.github.com>
2024-03-20 23:19:24 +0100

    beta3

--- js/script1.js
+++ js/script1.js
@@ -7,21 +7,29 @@ window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
 
 
-    if (scrollPosition > 0) { // Change this value according to when you want the animation to trigger
+    if (scrollPosition > 0) { 
         pcImage.classList.add('pc-home-scroll');
         pendriveImage.classList.add('pendrive-home-scroll');
-        TitleHome.classList.add('title-home-scroll')
+        TitleHome.classList.add('title-home-scroll');
     } else {
         pcImage.classList.remove('pc-home-scroll');
         pendriveImage.classList.remove('pendrive-home-scroll');
-        TitleHome.classList.remove('title-home-scroll')
+        TitleHome.classList.remove('title-home-scroll');
     }
 
-    // Aggiungi questa parte per far oscillare l'elemento infoshop
-    if (btnShopRect.top + btnShopRect.height > 0 && btnShopRect.top < this.window.innerHeight && scrollPosition > 0) {
-        btnShop.classList.add('infoshop-oscillate');
 
+    if (btnShopRect.top + btnShopRect.height > 0 && btnShopRect.top < window.innerHeight && scrollPosition > 0) {
+        btnShop.classList.add('infoshop-oscillate');
     } else {
         btnShop.classList.remove('infoshop-oscillate');
+    }
+
+    var cardContainer = document.querySelector('.card-container');
+    var cardContainerRect = cardContainer.getBoundingClientRect();
+
+    if (cardContainerRect.top + cardContainerRect.height > 0 && cardContainerRect.top < window.innerHeight && scrollPosition > 0) {
+        cardContainer.classList.add('in-view');
+    } else {
+        cardContainer.classList.remove('in-view');
     }
 });
