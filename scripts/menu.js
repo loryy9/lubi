8f300fed87c5b368158e4beeb312191078035686
loryy9 <102917754+loryy9@users.noreply.github.com>
2024-06-12 19:27:07 +0200

    beta2.2

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const navbar = document.querySelector('.navbar');
+    const links = document.querySelectorAll('.menu-links a'); // Seleziona tutti i link del menu
 
     menuToggle.addEventListener('change', function() {
         if (menuToggle.checked) {
@@ -8,5 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             navbar.classList.remove('menu-open');
         }
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            menuToggle.checked = false; // Deseleziona il checkbox
+            navbar.classList.remove('menu-open'); // Rimuove la classe 'menu-open'
+        });
     });
 });
